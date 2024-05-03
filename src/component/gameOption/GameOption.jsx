d25a085d155bd66e0b65e27a6fdaed4568154af6
Carlos Fermino <brutosacf@gmail.com>
2024-05-02 21:47:19 -0300

    Criado os components Game e GameOption

---
+++ src/component/gameOption/GameOption.jsx
@@ -0,0 +1,10 @@
+import style from './GameOption.module.css'
+
+function GameOpition () {
+  return (
+    <div className={style.gameOption}>
+      
+    </div>
+  )
+}
+export default GameOpition
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -3,6 +3,7 @@ import style from './Header.module.css'
 import Title from '../tilte/Title'
 import SubTitle from '../subTitle/SubTitle'
 import Icon from '../icon/Icon'
+import GameOption from '../gameOption/GameOption'
 
 function Header () {
   return (
@@ -12,6 +13,7 @@ function Header () {
       <SubTitle>Criado por Carlos Fermino</SubTitle>
       <div className={style.iconContent}>
       <Icon iconName="github" href="github.com/carlosfermino" />
+      
       </div>
     </div>
 
